4e9eed5710ab9d2430067cb0b79bf575e1d605a7
Thanaphat <sukchuen_t@su.ac.th>
2025-08-24 09:18:19 +0000

    week5-assignment

---
+++ week5-lab2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+func getUsers(c *gin.Context) {
+	user := []User{{ID: "1", Name: "Kanisorn"}}
+	c.JSON(200, user)
+}
+func main() {
+	r := gin.Default()
+	r.GET("/users", getUsers)
+	r.Run(":8080")
+}
